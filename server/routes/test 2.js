8a41a2be4a3d67e53b800735d663bfc41abf79b5
vivek pawar <vivekpawarvp07@gmail.com>
2024-09-28 17:37:59 +0530

    changes 28-sep-2024

--- server/routes/test 2.js
+++
@@ -1,8 +0,0 @@
-const ensureAuth = require('../middlewares/procted');
-const router = require('express').Router();
-router.get("/test",ensureAuth,(req,res)=>{
-    res.status(200).json({
-        message: "you got access to the data"
-    })
-})
-module.exports = router;
